8e6efb2acbb0270c3914594403f771ab466264fe
tuhinssam <tuhinssam@gmail.com>
2020-08-06 12:58:14 +0530

    added bootstrap in employee and registration pages

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ app.config['MYSQL_HOST'] = db['mysql_host']
 app.config['MYSQL_USER'] = db['mysql_user']
 app.config['MYSQL_PASSWORD'] = db['mysql_password']
 app.config['MYSQL_DB'] = db['mysql_db']
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 mysql = MySQL(app)
 
 @app.route('/', methods=['GET', 'POST'])
